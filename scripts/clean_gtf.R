007521068e206eb7c8dbb00ce034891758948068
Vinay Swamy <swamyvinny@gmail.com>
2018-10-17 16:14:25 -0400

    added tissue origin for tx and fixed gtf formatting

--- scripts/clean_gtf.R
+++ scripts/clean_gtf.R
@@ -3,7 +3,39 @@ library(rtracklayer)
 library(dplyr)
 library(biomaRt)
 gtf <- readGFF('ref/comb.gtf')
+dups <- filter(gtf, type=='transcript')%>%.[duplicated(.[,1:7]),'transcript_id']
+problem_tx <-filter(gtf,transcript_id%in%dups)
+gtf <- filter(gtf,!transcript_id%in%dups)
 ###this script is bad and should feel bad
+# for the sake of simplicity, lets remove duplicate transcripts from these each of the GTFs, then join together by transcript
+# exons don't wont have tissue info, but really who cares about them anyway
+read_GTF <- function(name){
+    gff<- readGFF(name)%>%dplyr::select(c("seqid","type","start","end","strand","transcript_id"))
+    dups <- filter(gff, type=='transcript')%>%.[duplicated(.[,1:5]),'transcript_id']
+    gff <- filter(gff,!transcript_id%in%dups)
+    return(gff)
+}
+
+gtf.tx <- filter(gtf,type=='transcript')
+ret <- read_GTF('ref/Retina_st.gtf')
+rpe <- read_GTF('ref/RPE_st.gtf')
+cor <- read_GTF('ref/Cornea_st.gtf')
+esc <- read_GTF('ref/ESC_st.gtf')
+pod <- read_GTF('ref/body_st.gtf')
+col <- colnames(gtf)
+gtf.tx_tissue_specific<- left_join(filter(gtf,type=='transcript'),filter(ret,type=='transcript'),by=c("seqid",'type',"start","end","strand"))%>%
+    left_join(filter(rpe,type=='transcript'),by=c("seqid",'type',"start","end","strand"))%>%
+    left_join(filter(cor,type=='transcript'),by=c("seqid",'type',"start","end","strand"))%>%
+    left_join(filter(esc ,type=='transcript'),by=c("seqid",'type',"start","end","strand"))#%>%
+    # left_join(filter(pod,type=='transcript'),by=c("seqid",'type',"start","end","strand"))
+# add uncooperative tx's back in
+problem_tx <- data.frame(problem_tx,matrix(data = NA,nrow = nrow(problem_tx),ncol = 4))
+colnames(gtf.tx_tissue_specific) <- colnames(problem_tx) <- c(col,'Retina','RPE','Cornea','ESC')
+gtf <- left_join(gtf,gtf.tx_tissue_specific,by=col)
+
+colnames(gtf) <- colnames(problem_tx) <- c(col,'Retina','RPE','Cornea','ESC')
+gtf <- rbind(gtf,problem_tx,stringsAsFactors=F)
+gdata::keep(gtf,sure=T)
 #remove single exon transcripts
 tx_counts <- table(gtf$transcript_id)
 keep <- names(tx_counts[tx_counts>2])
--- scripts/clean_gtf_part2.R
+++ scripts/clean_gtf_part2.R
@@ -2,9 +2,11 @@ library(rtracklayer)
 library(dplyr)
 library(biomaRt)
 load("/data/swamyvs/eyeintegration_splicing/tmp.RData")
+# load and format bedtools output
 genenames <- read.table('testout.bed',stringsAsFactors = F,header = F)%>%arrange(desc(V13))%>%
-    rename(transcript_id='V4',ext_gene_name='V10')
+    dplyr::rename(transcript_id='V4',ext_gene_name='V10')
 genenames <- genenames[!duplicated(genenames[,1:3]),]
+
 genenames$type <- 'transcript'
 gtf$type <- as.character(gtf$type)
 out_gtf <- left_join(gtf.tmp,genenames,by=c('type','transcript_id'))
@@ -12,25 +14,30 @@ out_gtf <- left_join(gtf.tmp,genenames,by=c('type','transcript_id'))
 #add genenames back in
 targ_tx <- genenames$transcript_id
 targ_genes <- genenames$ext_gene_name
+#exons are important again, could be a lot faster
 for(k in 1:length(targ_tx)){
     out_gtf[out_gtf$transcript_id==targ_tx[k],'gene_name'] <- targ_genes[k]
     
 }
+#add remaining gene names,
 out_gtf[is.na(out_gtf$gene_name),'gene_name'] <- out_gtf[is.na(out_gtf$gene_name),'gene_id']
-out_gtf <- out_gtf[,1:12]
-out_gtf[is.na(out_gtf)] <- '.'
+out_gtf <- out_gtf[,1:17]
+#out_gtf[is.na(out_gtf)] <- '.'
 
+# now add back the remaining gene features , not sure if this is important at all but might as well
+ # ref_genes <- filter(ref_gtf,type=='gene')
+ # ref_genes <- ref_genes[ref_genes$gene_name%in%gtf$gene_name,  ]
+ # ref_genes <- ref_genes[,c(1:9,14,11,)]
+ # cn <- c("seqid", "source", "type", "start", "end" ,"score", "strand", "phase" ,"gene_id", "transcript_id" ,"gene_name"    )
+ # colnames(out_gtf) <- colnames(ref_genes) <- cn
+#gtf_final <- rbind(out_gtf,ref_genes)
+gtf_final <- out_gtf
+# identify what transcripts are novel
+gtf_final$added_by_stringtie=!grepl('ENST',gtf_final$transcript_id)
 
-ref_genes <- filter(ref_gtf,type=='gene')
-ref_genes <- ref_genes[ref_genes$gene_name%in%gtf$gene_name,  ]
-ref_genes <- ref_genes[,c(1:9,14,11,20)]
-cn <- c("seqid", "source", "type", "start", "end" ,"score", "strand", "phase" ,"gene_id", "transcript_id" ,"gene_name", "exon_number"    )
-colnames(out_gtf) <- colnames(ref_genes) <- cn
+#gtf_final[is.na(gtf_final)] <- '.'
+to_write <- lapply(colnames(gtf_final)[9:17],function(x)paste0(x,' "',gtf_final[,x],'" '))
 
-gtf_final <- rbind(out_gtf,ref_genes)
-gtf_final$added_by_stringtie=grepl('ENST',gtf_final$transcript_id)
-
-gtf_final[is.na(gtf_final)] <- '.'
 to_write <- paste( paste0('gene_id ',gtf_final$gene_id),
                    paste0(' transcript_id ','"',gtf_final$transcript_id,'"'),
                    paste0(' exon_numer ','"',gtf_final$exon_number,'"'),

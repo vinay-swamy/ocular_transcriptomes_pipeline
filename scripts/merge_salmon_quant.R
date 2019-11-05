5457b2bccb6eb0e15371cae645603ce56937f114
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2019-11-05 14:23:59 -0500

    all we do now load all tissue exp files and full join

--- scripts/merge_salmon_quant.R
+++ scripts/merge_salmon_quant.R
@@ -1,32 +1,13 @@
 library(tidyverse)
 library(tximport)
-library(parallel)
-source('~/scripts/read_salmon.R')
-args <- c('/data/swamyvs/eyeintegration_splicing/', 'sampleTableV6.tsv', 'data/rawST_tx_quant_files/',
-          'data/misc/TCONS2MSTRG.tsv', 'data/exp_files/all_tissue_quant.tsv.gz')
 args <- commandArgs(trailingOnly = F)
 wd <- args[1]
-sample_file <- args[2]
-path_to_quant <- args[3]
-tcons2mstrg_file <- args[4]
-out_exp_file <- args[5]
+path_to_quant <- args[2]
+out_exp_file <- args[3]
 
-
-
-read_quant <- function(path, s_tissue){
-    quant <- read_salmon(path) %>% rename(!!s_tissue := transcript_id)
-}
-
-sample_table <- read_tsv(sample_file)
-subtissues <- unique(sample_table$subtissue)
-quant_paths <- paste0(path_to_quant, subtissues)
-tc2ms <- read_tsv(tcons2mstrg_file)
-all_quant <- mclapply(seq_along(subtissues), function(i) read_quant(quant_paths[i], subtissues[i]), mc.cores = 12) %>% 
-    reduce(left_join, .init =tc2ms)
-cols <- colnames(all_quant)
-cols_to_keep <-  c('transcript_id', cols[cols %in% sample_table$sample])
-all_quant <- all_quant[,cols_to_keep]
-
+exp_files <- list.files(path_to_quant, pattern = '.Rdata', recursive = T, full.names = T)
+all_quant <- lapply(exp_files, load) %>% reduce(full_join)
+save(all_quant, file = out_exp_file)
 
 
 

737b06fa7a8ed977e885286648a17ac6d7de75b9
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2019-12-02 20:46:46 -0500

    using RDS instead of Rdata files

--- scripts/merge_salmon_quant.R
+++ scripts/merge_salmon_quant.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(tximport)
-args <- commandArgs(trailingOnly = F)
+args <- commandArgs(trailingOnly = T)
 wd <- args[1]
 path_to_quant <- args[2]
 out_exp_file <- args[3]
-
-exp_files <- list.files(path_to_quant, pattern = '.Rdata', recursive = T, full.names = T)
-all_quant <- lapply(exp_files, load) %>% reduce(full_join)
+setwd(wd)
+exp_files <- list.files(path_to_quant, pattern = '.RDS', recursive = T, full.names = T)
+all_quant <- lapply(exp_files, readRDS) %>% reduce(full_join)
 save(all_quant, file = out_exp_file)
 
 

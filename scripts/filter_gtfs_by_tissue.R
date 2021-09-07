a5477643b784f5be9271920a62bff6f29eab1946
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2021-09-07 14:33:09 -0400

    forgot to add to repo when i was refactoring yikes

---
+++ scripts/filter_gtfs_by_tissue.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(data.table)
+library(parallel)
+library(glue)
+library(argparse)
+parser <- ArgumentParser()
+parser$add_argument('--workingDir', action = 'store', dest = 'working_dir')
+parser$add_argument('--sampleTable', action = 'store', dest = 'sample_table_file')
+parser$add_argument('--subtissue', action = 'store', dest = 't_subtissue')
+parser$add_argument('--gtfDir', action = 'store', dest = 'prefix_for_gtfs')
+parser$add_argument('--stringtieQuant', action = 'store', dest = 'path_to_stringtie_gtfs')
+##########
+# working_dir='/data/swamyvs/ocular_transcriptomes_pipeline/'
+# sample_table_file="sampleTableFullV3.tsv"             
+# t_subtissue="Thyroid"             
+# prefix_for_gtfs="data/gtfs/raw_tissue_gtfs/Thyroid"
+# path_to_stringtie_gtfs="st_out/"
+#############3    
+
+list2env(parser$parse_args(), .GlobalEnv)
+
+
+source('scripts/write_gtf.R')
+source('scripts/shared_funcs.R')
+
+track_file <- glue('{prefix_for_gtfs}.tracking')
+raw_merged_gtf <- glue('{prefix_for_gtfs}.combined.gtf')
+filtered_convtab_file <- glue('{prefix_for_gtfs}.convtab')
+filtered_gtf_file <-  glue('{prefix_for_gtfs}.combined.filtered.gtf')
+
+
+
+setwd(working_dir)
+
+read_stringtie <- function(file, name ){
+    df <- data.table::fread(file, sep='\t', header = F) %>% as_tibble %>% filter(V3 == "transcript")
+    if (nrow(df) == 0) {return(tibble())}
+    tibble(
+        !!name := str_extract(df$V9, 'transcript_id \".+\";') %>% str_split('\"') %>% sapply(function(x)x[2]),
+        TPM=str_extract(df$V9, 'TPM \".+\";') %>% str_split('\"') %>% sapply(function(x)x[2]) %>% as.numeric 
+    )
+    
+}
+
+#### process track file into an id conversion table 
+
+conv_tab <- read_track_file(track_file)
+
+#### read stringtie quantification
+samples <- read_tsv(sample_table_file) %>% filter(subtissue  == t_subtissue) %>% pull(sample)
+st_files <- paste0(path_to_stringtie_gtfs, samples, '.gtf')
+all(file.exists(st_files))
+stringtie_quant <- lapply(seq_along(st_files), function(i) read_stringtie(st_files[i], samples[i]) %>% 
+                              inner_join(conv_tab[c('transcript_id', samples[i])]) %>% 
+                              select(-(!!samples[i]))  %>% rename(!!samples[i]:=TPM)) %>% 
+    reduce(full_join) %>% 
+    select(transcript_id, everything())
+stringtie_quant[is.na(stringtie_quant)] <- 0
+
+
+quant_filtered <- stringtie_quant %>% filter(rowMeans(.[,samples])>=1)
+conv_tab_filtered <- conv_tab %>% filter(transcript_id %in% quant_filtered$transcript_id)
+gtf <- rtracklayer::readGFF(raw_merged_gtf)
+filtered_gtf <- gtf %>% filter(transcript_id %in% conv_tab_filtered$transcript_id)
+write_gtf3(filtered_gtf, filtered_gtf_file)
+fwrite(conv_tab_filtered, file = filtered_convtab_file, sep = '\t')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
